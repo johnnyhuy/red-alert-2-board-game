8cafcd121a7e536771e354b0438bb81e0d35c8fe
rmit-s3670659-udahakapugedara-senadhi <37611003+rmit-s3670659-udahakapugedara-senadhi@users.noreply.github.com>
2019-03-21 13:23:14 +1100

    Update ScoutZombie.java

--- src/models/units/ScoutZombie.java
+++ src/models/units/ScoutZombie.java
@@ -1,4 +1,23 @@
 package models.units;
 
+import models.board.Hexagon;
+import models.player.Player;
+
 public class ScoutZombie extends Unit {
+
+	public ScoutZombie(Hexagon location, Player player) {
+		super(location, player);
+		
+	}
+	
+
+	public ScoutZombie() {
+		
+	}
+
+
+	@Override
+	public void setWinnables() {
+		super.winnables.add(new Zombat());					
+	}
 }
