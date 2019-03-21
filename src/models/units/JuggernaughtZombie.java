9556f2986029e7ddb7bf3845066cd3d39267dffc
rmit-s3670659-udahakapugedara-senadhi <37611003+rmit-s3670659-udahakapugedara-senadhi@users.noreply.github.com>
2019-03-21 13:22:23 +1100

    Update JuggernaughtZombie.java

--- src/models/units/JuggernaughtZombie.java
+++ src/models/units/JuggernaughtZombie.java
@@ -1,4 +1,25 @@
 package models.units;
 
-public class JuggernaughtZombie {
+import models.board.Hexagon;
+import models.player.Player;
+
+public class JuggernaughtZombie extends Unit{
+
+	public JuggernaughtZombie(Hexagon location, Player player) {
+		super(location, player);			
+	}
+
+	public JuggernaughtZombie() {
+		
+	}
+
+	@Override
+	public void setWinnables() {
+		super.winnables.add(new Zombat());
+		super.winnables.add(new ScoutZombie());
+		super.winnables.add(new Soldier());		
+	}
+	
+	
+	
 }
