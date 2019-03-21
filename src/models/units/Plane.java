e508aa703e30904ab2bb7d79c92b3f92e43a117f
rmit-s3670659-udahakapugedara-senadhi <37611003+rmit-s3670659-udahakapugedara-senadhi@users.noreply.github.com>
2019-03-21 13:22:49 +1100

    Update Plane.java

--- src/models/units/Plane.java
+++ src/models/units/Plane.java
@@ -1,4 +1,22 @@
 package models.units;
 
+import models.board.Hexagon;
+import models.player.Player;
+
 public class Plane extends Unit {
+
+	public Plane(Hexagon location, Player player) {
+		super(location, player);
+		
+	}
+
+	@Override
+	public void setWinnables() {
+		super.winnables.add(new Zombat());
+		super.winnables.add(new ScoutZombie());
+		super.winnables.add(new Soldier());	
+		super.winnables.add(new JuggernaughtZombie());
+		super.winnables.add(new Tank());
+		
+	}
 }
