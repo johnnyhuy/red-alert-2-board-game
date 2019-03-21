91aca6b9297d0f7766ad76666f103be8c9d90c31
rmit-s3670659-udahakapugedara-senadhi <37611003+rmit-s3670659-udahakapugedara-senadhi@users.noreply.github.com>
2019-03-21 13:24:08 +1100

    Update Tank.java

--- src/models/units/Tank.java
+++ src/models/units/Tank.java
@@ -1,4 +1,25 @@
 package models.units;
 
+import models.board.Hexagon;
+import models.player.Player;
+
 public class Tank extends Unit {
+
+	public Tank(Hexagon location, Player player) {
+		super(location, player);
+		
+	}
+
+	public Tank() {
+		
+	}
+
+	@Override
+	public void setWinnables() {
+		super.winnables.add(new Zombat());
+		super.winnables.add(new ScoutZombie());
+		super.winnables.add(new Soldier());	
+		super.winnables.add(new JuggernaughtZombie());
+		
+	}
 }
