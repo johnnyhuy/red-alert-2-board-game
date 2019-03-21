96a826f7f8265f2c6bae125f87b8785a93941a6d
rmit-s3670659-udahakapugedara-senadhi <37611003+rmit-s3670659-udahakapugedara-senadhi@users.noreply.github.com>
2019-03-21 13:23:45 +1100

    Update Soldier.java

--- src/models/units/Soldier.java
+++ src/models/units/Soldier.java
@@ -1,4 +1,24 @@
 package models.units;
 
+import models.board.Hexagon;
+import models.player.Player;
+
 public class Soldier extends Unit {
+
+	public Soldier(Hexagon location, Player player) {
+		super(location, player);
+		
+	}
+
+	public Soldier() {
+		
+	}
+
+	@Override
+	public void setWinnables() {
+		super.winnables.add(new Zombat());
+		super.winnables.add(new ScoutZombie());
+			
+		
+	}
 }
