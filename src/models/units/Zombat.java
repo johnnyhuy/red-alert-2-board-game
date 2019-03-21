24a80ef0dba1660f107df9cbbae632bfd660b284
rmit-s3670659-udahakapugedara-senadhi <37611003+rmit-s3670659-udahakapugedara-senadhi@users.noreply.github.com>
2019-03-21 13:25:01 +1100

    Update Zombat.java

--- src/models/units/Zombat.java
+++ src/models/units/Zombat.java
@@ -1,4 +1,22 @@
 package models.units;
 
+import models.board.Hexagon;
+import models.player.Player;
+
 public class Zombat extends Unit {
+
+	public Zombat(Hexagon location, Player player) {
+		super(location, player);
+		
+	}
+
+	public Zombat() {
+		
+	}
+
+	@Override
+	public void setWinnables() {
+		//no winnables
+		
+	}
 }
