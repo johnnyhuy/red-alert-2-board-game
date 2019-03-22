eb1659eb9612ec49c50c8b03302b79ea80a9d0aa
cheeweilow <s3604353@student.rmit.edu.au>
2019-03-22 22:52:15 +1100

    Added Team /  Player objects.

--- src/models/player/Player.java
+++ src/models/player/Player.java
@@ -1,4 +1,33 @@
 package models.player;
+import models.units.Unit;
+import java.util.List;
 
-public class Player {
+public class Player {	
+	private String playerName;
+	private Team team;
+	private List<Unit> units;
+
+	public Player(String playerName, Team team, List<Unit> units) 
+	{
+		super();
+		this.playerName = playerName;
+		this.team = team;
+		this.units = units;
+	}
+
+	public String getPlayerName() {
+		return playerName;
+	}
+
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+
+	public Team getTeam() {
+		return team;
+	}
+
+	public List<Unit> getUnits() {
+		return units;
+	}
 }
---
+++ src/models/player/Team.java
@@ -0,0 +1,7 @@
+package models.player;
+
+public enum Team {
+
+	RED, BLUE;
+	
+}
