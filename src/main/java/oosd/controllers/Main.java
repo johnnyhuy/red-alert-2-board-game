b0106544a526c4635ce273666657d4aec528a07a
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-03-24 09:34:50 +1100

    Got a hexagon board going.

--- src/main/java/oosd/controllers/Main.java
+++ src/main/java/oosd/controllers/Main.java
@@ -1,17 +1,64 @@
 package oosd.controllers;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Polygon;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+//        Parent root = FXMLLoader.load(getClass().getResource("board.fxml"));
+//        primaryStage.setTitle("OOSD Board");
+//        primaryStage.setScene(new Scene(root, 300, 275));
+//        primaryStage.show();
+
+        AnchorPane tileMap = new AnchorPane();
+        Scene content = new Scene(tileMap, 700, 700);
+        primaryStage.setScene(content);
+
+        final double scale = 100;
+        double height = 6 * scale;
+        double width = 3 * scale;
+
+        final double tHeight = Math.sqrt(3);
+        final double size = scale / 2;
+
+        double xScale = size * (tHeight / 2.0), yScale = size * tHeight;
+        double xOrigin, yOrigin, yOffset;
+        double distanceBetween = size * (3.0 / 2.0);
+
+        for (yOrigin = scale; yOrigin < height; yOrigin += yScale)
+        {
+            for (xOrigin = scale, yOffset = yOrigin; xOrigin < width; xOrigin += distanceBetween)
+            {
+                Polygon tile = new Polygon();
+                tile.getPoints().addAll(
+                    xOrigin, yOffset,
+                    xOrigin + size, yOffset,
+                    xOrigin + distanceBetween, yOffset + xScale,
+                    xOrigin + size, yOffset + yScale,
+                    xOrigin, yOffset + yScale,
+                    xOrigin - (size / 2.0), yOffset + xScale
+                );
+
+                Text text = new Text();
+                text.setFont(new Font(20));
+                text.setText("test");
+
+                tile.setFill(Paint.valueOf("#ffffff"));
+                tile.setStrokeWidth(2);
+                tile.setStroke(Paint.valueOf("#000000") );
+                tileMap.getChildren().add(tile);
+                tileMap.getChildren().add(text);
+
+                yOffset = yOffset == yOrigin ? yOffset + xScale : yOrigin;
+            }
+        }
         primaryStage.show();
     }
 
--- src/main/java/oosd/models/board/Hexagon.java
+++ src/main/java/oosd/models/board/Hexagon.java
@@ -1,4 +1,21 @@
 package oosd.models.board;
 
 public class Hexagon {
+    private double[] points;
+    private double center;
+
+    public Hexagon(double side){
+        center = ((Math.sqrt(3)/2)*side);
+        points = new double[12];
+        points[0] = 0.0;           points[1] = 0.0;
+        points[2] = side;          points[3] = 0.0;
+        points[4] = side+(side/2); points[5] = center;
+        points[6] = side;          points[7] = center * 2;
+        points[8] = 0.0;           points[9] = center * 2;
+        points[10] = -side/2;      points[11] = center;
+    }
+
+    public double [] getPoints(){
+        return points;
+    }
 }
