1abe5a30cb559ef5d7444d91d571a0a8d0c57380
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-03-24 22:43:48 +1100

    Moved board creation to game engine.

--- src/main/java/oosd/models/GameEngine.java
+++ src/main/java/oosd/models/GameEngine.java
@@ -1,4 +1,15 @@
 package oosd.models;
 
+import oosd.models.board.Board;
+
 public class GameEngine {
+    private Board board;
+
+    public GameEngine() {
+        this.board = new Board(6, 6);
+    }
+
+    public Board getBoard() {
+        return this.board;
+    }
 }
