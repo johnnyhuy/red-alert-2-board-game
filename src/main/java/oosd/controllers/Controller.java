f44844d5ae9a9362ef23e3911c7e5c5317ec830b
Johnny Huynh <johnny.huynh@enett.com>
2019-03-26 07:56:21 +1100

    Reverted back to game controller ctor.

--- src/main/java/oosd/controllers/Controller.java
+++ src/main/java/oosd/controllers/Controller.java
@@ -1,13 +1,5 @@
 package oosd.controllers;
 
-import oosd.models.GameEngine;
-
 abstract class Controller {
-    protected final GameEngine gameEngine;
-
-    Controller(GameEngine gameEngine) {
-        this.gameEngine = gameEngine;
-    }
-
     abstract void initialize();
 }
