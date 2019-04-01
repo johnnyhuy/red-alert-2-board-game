4d34688e474f7010cb53551f9e31e9079347e44a
Johnny Huynh <thejohnnyhuy@gmail.com>
2019-04-01 18:54:56 +1100

    Added the rest of the units.

--- src/main/java/oosd/models/GameEngine.java
+++ src/main/java/oosd/models/GameEngine.java
@@ -4,14 +4,13 @@ import oosd.models.board.Board;
 import oosd.models.board.Hexagon;
 import oosd.models.player.Player;
 import oosd.models.player.Team;
-import oosd.models.units.ScoutZombie;
-import oosd.models.units.Unit;
+import oosd.models.units.*;
 
 public class GameEngine {
     private Board board;
 
     public GameEngine() {
-        final int boardRow = 4;
+        final int boardRow = 6;
         final int boardColumn = 6;
 
         this.board = new Board(boardColumn, boardRow);
@@ -20,8 +19,14 @@ public class GameEngine {
     public void initialize() {
         Hexagon[][] hexagons = this.board.getHexagons();
         Player playerOne = new Player("Johnny Dave", Team.RED);
+        Player playerTwo = new Player("Jane Doe", Team.BLUE);
 
-        Unit scoutZombie = new ScoutZombie(hexagons[1][1], playerOne);
+        Unit tank = new Tank(hexagons[0][0], playerOne);
+        Unit plane = new Plane(hexagons[1][0], playerOne);
+        Unit soldier = new Soldier(hexagons[2][0], playerOne);
+        Unit juggernautZombie = new JuggernautZombie(hexagons[0][5], playerOne);
+        Unit scoutZombie = new ScoutZombie(hexagons[1][5], playerOne);
+        Unit zombat = new Zombat(hexagons[2][5], playerOne);
     }
 
     public Board getBoard() {
