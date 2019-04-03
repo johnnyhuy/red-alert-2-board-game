7b136bf8ca60bc73771873ec2512f230a5ae6b6f
Johnny Huynh <thejohnnyhuy@gmail.com>
2019-04-03 23:05:54 +1100

    Moved UI creation logic into a factory.

--- src/main/java/oosd/views/BoardView.java
+++ src/main/java/oosd/views/BoardView.java
@@ -8,37 +8,37 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Polygon;
 import javafx.scene.text.Text;
 import oosd.controllers.GameController;
+import oosd.factories.BoardFactory;
 import oosd.models.GameEngine;
 import oosd.models.board.Board;
 import oosd.models.board.Hexagon;
 
 public class BoardView extends View {
+    private final Board board;
     private final GameController controller;
-    private final GameEngine gameEngine;
     private final AnchorPane boardPane;
     private final Polygon[][] hexagonPolygons;
-    private final Board board;
-
-    private final double size = 50;
-    private final double gap = size * 1.5;
-    private final double tHeight = Math.sqrt(3);
-    private final double halfIncrement = size * (tHeight / 2.0);
-    private final double fullIncrement = size * tHeight;
+    private final Text[][] unitText;
+    private final Circle[][] unitCircles;
+    private final BoardFactory boardFactory;
 
     public BoardView(GameController controller, GameEngine gameEngine, AnchorPane boardPane) {
         this.controller = controller;
-        this.gameEngine = gameEngine;
         this.boardPane = boardPane;
-        this.board = this.gameEngine.getBoard();
-        this.hexagonPolygons = new Polygon[board.getColumns()][board.getRows()];
+        board = gameEngine.getBoard();
+
+        this.boardFactory = new BoardFactory(board.getColumns(), board.getRows());
+        this.hexagonPolygons = boardFactory.createHexagon();
+        this.unitText = boardFactory.createUnitText();
+        this.unitCircles = boardFactory.createUnitCircle();
     }
 
     public void selectUnit(Hexagon previousHexagon, Hexagon hexagon) {
         if (previousHexagon != null) {
-            this.hexagonPolygons[previousHexagon.getColumn()][previousHexagon.getRow()].setFill(Paint.valueOf("#ffffff"));
+            hexagonPolygons[previousHexagon.getColumn()][previousHexagon.getRow()].setFill(Paint.valueOf("#ffffff"));
         }
 
-        this.hexagonPolygons[hexagon.getColumn()][hexagon.getRow()].setFill(Paint.valueOf("#dadada"));
+        hexagonPolygons[hexagon.getColumn()][hexagon.getRow()].setFill(Paint.valueOf("#dadada"));
     }
 
     @Override
@@ -47,66 +47,42 @@ public class BoardView extends View {
 
         int xOffset = 80;
         int yOffset = 80;
-        int hexagonCount = 0;
         double x = 0;
         double y = 0;
+        int hexagonCount = 0;
+        int rows = board.getRows();
+        int columns = board.getColumns();
 
-        for (int yIndex = 0; yIndex < board.getRows(); yIndex++) {
-            for (int xIndex = 0; xIndex < board.getColumns(); xIndex++) {
+        for (int yIndex = 0; yIndex < rows; yIndex++) {
+            for (int xIndex = 0; xIndex < columns; xIndex++) {
                 Hexagon hexagon = hexagons[xIndex][yIndex];
 
-                Polygon hexagonPolygon = DrawHexagon(x, y, hexagon);
-                this.hexagonPolygons[xIndex][yIndex] = hexagonPolygon;
-
-                Circle unitCircle = new Circle();
-                unitCircle.setRadius(size / 4);
-                unitCircle.setFill(Paint.valueOf("#DADADA"));
                 double unitOpacity = hexagon.getUnit() != null ? 1 : 0;
-                unitCircle.setOpacity(unitOpacity);
+                unitCircles[xIndex][yIndex].setOpacity(unitOpacity);
 
                 String name = hexagon.getUnit() != null ? hexagon.getUnit().getName() : "";
-                final Text text = new Text();
-                text.setText(name);
+                unitText[xIndex][yIndex].setText(name);
 
                 final StackPane stack = new StackPane();
-                stack.getChildren().addAll(hexagonPolygon, unitCircle, text);
+                stack.getChildren().addAll(hexagonPolygons[xIndex][yIndex], unitCircles[xIndex][yIndex], unitText[xIndex][yIndex]);
                 stack.setLayoutX(xOffset + x);
                 stack.setLayoutY(yOffset + y);
                 stack.setAlignment(Pos.CENTER);
 
-                this.boardPane.getChildren().add(stack);
+                boardPane.getChildren().add(stack);
 
-                // Every even element set the y value down
                 if (hexagonCount % 2 == 0) {
-                    y = y + halfIncrement;
+                    y = y + boardFactory.getHalfIncrement();
                 } else {
-                    y = y - halfIncrement;
+                    y = y - boardFactory.getHalfIncrement();
                 }
 
                 hexagonCount++;
 
-                x = xIndex == board.getColumns() - 1 ? 0 : x + gap;
+                x = xIndex == columns - 1 ? 0 : x + boardFactory.getGap();
             }
 
-            y = yIndex == board.getRows() - 1 ? 0 : y + fullIncrement;
+            y = yIndex == rows - 1 ? 0 : y + boardFactory.getFullIncrement();
         }
-    }
-
-    private Polygon DrawHexagon(double x, double y, Hexagon hexagon) {
-        Polygon hexagonPolygon = new Polygon();
-        hexagonPolygon.setOnMouseClicked(event -> controller.board(event, hexagon));
-        hexagonPolygon.setFill(Paint.valueOf("#ffffff"));
-        hexagonPolygon.setStrokeWidth(2);
-        hexagonPolygon.setStroke(Paint.valueOf("#000000"));
-        hexagonPolygon.getPoints().addAll(
-                x, y,
-                x + size, y,
-                x + gap, y + halfIncrement,
-                x + size, y + fullIncrement,
-                x, y + fullIncrement,
-                x - (size / 2.0), y + halfIncrement
-        );
-
-        return hexagonPolygon;
     }
 }
