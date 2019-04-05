55a82f0282fd351c0b7ede5554ed9886740e4ec1
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-04-06 00:19:33 +1100

    WIP Refactoring.

--- src/main/java/oosd/models/GameEngine.java
+++ src/main/java/oosd/models/GameEngine.java
@@ -100,28 +100,15 @@ public class GameEngine {
 
         for (int move = 1; move <= hexagon.getUnit().getMove(); move++) {
             int north = hexagon.getRow() - move;
-            if (north >= board.getRows() || north < 0) {
-                continue;
-            }
-
-            if (board.getHexagon(hexagon.getColumn(), north).getUnit() != null) {
-                break;
-            }
-
-            validMoves.add(new Hexagon(hexagon.getColumn(), north));
-        }
-
-        for (int move = 1; move <= hexagon.getUnit().getMove(); move++) {
             int south = hexagon.getRow() + move;
-            if (south >= board.getRows()) {
-                continue;
+
+            if (north < board.getRows() && north > 0 && board.getHexagon(hexagon.getColumn(), north).getUnit() == null) {
+                validMoves.add(new Hexagon(hexagon.getColumn(), north));
             }
 
-            if (board.getHexagon(hexagon.getColumn(), south).getUnit() != null) {
-                break;
+            if (south < board.getRows() && board.getHexagon(hexagon.getColumn(), south).getUnit() == null) {
+                validMoves.add(new Hexagon(hexagon.getColumn(), south));
             }
-
-            validMoves.add(new Hexagon(hexagon.getColumn(), south));
         }
 
         for (int move = 1; move <= hexagon.getUnit().getMove(); move++) {
