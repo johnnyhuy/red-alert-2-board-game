aa670a62781757624ecfb1b72b58c1e51fa62dd5
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-04-06 10:59:26 +1100

    Moved back to abstract class.

--- src/main/java/oosd/views/BoardView.java
+++ src/main/java/oosd/views/BoardView.java
@@ -16,7 +16,7 @@ import oosd.models.board.Hexagon;
  * SOLID:  Single Responsibility Principle
  * The view should only be responsible for managing the user interface (e.g. interacting with the JavaFX library)
  */
-public class BoardView implements View {
+public class BoardView extends View {
     private final Board board;
     private final GameController controller;
     private GameEngine gameEngine;
