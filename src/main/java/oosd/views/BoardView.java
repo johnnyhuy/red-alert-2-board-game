ef327f9928f8b62e32a7eb923761f0caf766ce7c
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-04-06 10:51:51 +1100

    Use view interface instead.

--- src/main/java/oosd/views/BoardView.java
+++ src/main/java/oosd/views/BoardView.java
@@ -16,7 +16,7 @@ import oosd.models.board.Hexagon;
  * SOLID:  Single Responsibility Principle
  * The view should only be responsible for managing the user interface (e.g. interacting with the JavaFX library)
  */
-public class BoardView extends View {
+public class BoardView implements Viewable {
     private final Board board;
     private final GameController controller;
     private GameEngine gameEngine;
--- src/main/java/oosd/views/View.java
+++ src/main/java/oosd/views/View.java
@@ -1,4 +1,5 @@
 package oosd.views;
 
-abstract class View {
+interface Viewable {
+    void initialize();
 }
