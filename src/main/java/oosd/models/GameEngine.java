fc94d9edd7defe20f8174399e5cf8e5bbc9acc75
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-04-06 17:27:10 +1100

    Moved game initialization stuff into the main class.

--- src/main/java/oosd/models/GameEngine.java
+++ src/main/java/oosd/models/GameEngine.java
@@ -3,50 +3,25 @@ package oosd.models;
 import oosd.models.board.Board;
 import oosd.models.board.Hexagon;
 import oosd.models.player.Player;
-import oosd.models.player.Team;
-import oosd.models.units.humans.Plane;
-import oosd.models.units.humans.Soldier;
-import oosd.models.units.humans.Tank;
-import oosd.models.units.zombies.JuggernautZombie;
-import oosd.models.units.zombies.ScoutZombie;
-import oosd.models.units.zombies.Zombat;
+
+import java.util.Iterator;
+import java.util.List;
 
 public class GameEngine {
     private Board board;
     private Hexagon selectedHexagon;
+    private Player turn;
+    private List<Player> players;
 
-    public GameEngine(int boardColumns, int boardRows) {
-        this.board = new Board(boardColumns, boardRows);
-    }
+    public GameEngine(Board board, List<Player> players) {
+        this.board = board;
+        this.players = players;
 
-    /**
-     * GRASP: The creator
-     * Responsible to initialize game pieces and players in the board.
-     */
-    public void initialize() {
-        Player playerOne = new Player("Johnny Dave", Team.RED);
-        Player playerTwo = new Player("Jane Doe", Team.BLUE);
-
-        board.getHexagon(0, 0).setUnit(new Soldier(playerOne));
-        board.getHexagon(1, 0).setUnit(new Soldier(playerOne));
-        board.getHexagon(2, 0).setUnit(new Tank(playerOne));
-        board.getHexagon(3, 0).setUnit(new Tank(playerOne));
-        board.getHexagon(4, 0).setUnit(new Plane(playerOne));
-        board.getHexagon(5, 0).setUnit(new Plane(playerOne));
-        board.getHexagon(6, 0).setUnit(new Tank(playerOne));
-        board.getHexagon(7, 0).setUnit(new Tank(playerOne));
-        board.getHexagon(8, 0).setUnit(new Soldier(playerOne));
-        board.getHexagon(9, 0).setUnit(new Soldier(playerOne));
-        board.getHexagon(0, 9).setUnit(new ScoutZombie(playerTwo));
-        board.getHexagon(1, 9).setUnit(new ScoutZombie(playerTwo));
-        board.getHexagon(2, 9).setUnit(new Zombat(playerTwo));
-        board.getHexagon(3, 9).setUnit(new Zombat(playerTwo));
-        board.getHexagon(4, 9).setUnit(new JuggernautZombie(playerTwo));
-        board.getHexagon(5, 9).setUnit(new JuggernautZombie(playerTwo));
-        board.getHexagon(6, 9).setUnit(new Zombat(playerTwo));
-        board.getHexagon(7, 9).setUnit(new Zombat(playerTwo));
-        board.getHexagon(8, 9).setUnit(new ScoutZombie(playerTwo));
-        board.getHexagon(9, 9).setUnit(new ScoutZombie(playerTwo));
+        // Whoever we add to the players list, the first one takes the turn
+        Player firstPlayer = players.get(0);
+        if (firstPlayer != null) {
+            this.turn = firstPlayer;
+        }
     }
 
     /**
