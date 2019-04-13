fff8fd62366bd4bed4b2f4e4a09ebc429766b9fb
cheeweilow <s3604353@student.rmit.edu.au>
2019-04-13 23:05:00 +1000

    Team Contracts done

---
+++ src/main/java/oosd/contracts/models/TeamContract.java
@@ -0,0 +1,41 @@
+package oosd.contracts.models;
+
+import de.vksi.c4j.Target;
+import de.vksi.c4j.ClassInvariant;
+import oosd.models.player.Team;
+
+import static de.vksi.c4j.Condition.*;
+
+public class TeamContract extends Team
+{
+	@Target
+	private Team target;
+	
+    @ClassInvariant
+    public void classInvariant()
+    {
+    	assert target.getName().length() > NAME_SIZE : "name > NAME_SIZE";
+    }
+	
+	public TeamContract(String name)
+	{
+		super(name);
+		
+		if(preCondition())
+		{
+			assert !name.isEmpty();
+		}
+	}
+	
+	@Override
+	public String getName()
+	{
+		if (preCondition())
+		{
+			assert target.getName().length() > NAME_SIZE : "name > NAME_SIZE";
+		}
+		return ignored();
+	}
+	
+
+}
--- src/main/java/oosd/models/player/Team.java
+++ src/main/java/oosd/models/player/Team.java
@@ -1,9 +1,14 @@
 package oosd.models.player;
 
+import de.vksi.c4j.ContractReference;
+import oosd.contracts.models.TeamContract;
+
 // TODO: Convert this to C4J
 // @invariant name.size() > 0
+@ContractReference(TeamContract.class)
 public class Team {
     private String name;
+    public int NAME_SIZE = 0;
 
     public Team(String name) {
         this.name = name;
