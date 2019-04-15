bafe1f18ab5ed565f7b359482cad58a0142f1b43
cheeweilow <s3604353@student.rmit.edu.au>
2019-04-15 18:45:05 +1000

    Added invariance to TeamContract
    Added invariance to catch other empty string types in case they get
    through

--- src/main/java/oosd/contracts/models/TeamContract.java
+++ src/main/java/oosd/contracts/models/TeamContract.java
@@ -1,6 +1,7 @@
 package oosd.contracts.models;
 
 import de.vksi.c4j.Target;
+import de.vksi.c4j.ClassInvariant;
 import oosd.models.player.Team;
 
 import static de.vksi.c4j.Condition.preCondition;
@@ -8,12 +9,19 @@ import static de.vksi.c4j.Condition.preCondition;
 public class TeamContract extends Team {
     @Target
     private Team target;
+    
+    @ClassInvariant
+    public void classInvariant() {
+    	assert !target.getName().isEmpty();
+    	assert target.getName().length() != 0;
+    }
 
     public TeamContract(String name) {
         super(name);
 
         if (preCondition()) {
             assert !name.isEmpty();
+        	assert name.length() != 0;
         }
     }
 }
