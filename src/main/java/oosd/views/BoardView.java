5f79797425b0b270175ad017eb1bf8aca8efc626
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-05-09 00:01:38 +1000

    MASSIVE REFACTOR! FEELS GOOD!

--- src/main/java/oosd/views/BoardView.java
+++ src/main/java/oosd/views/BoardView.java
@@ -1,169 +1,97 @@
 package oosd.views;
 
-import javafx.scene.Group;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.*;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Paint;
-import javafx.scene.shape.Polygon;
 import javafx.scene.text.Text;
 import oosd.controllers.GameController;
-import oosd.factories.ViewComponentFactory;
 import oosd.models.GameEngine;
 import oosd.models.board.Board;
 import oosd.models.board.Piece;
-import oosd.views.components.PieceViewComponent;
+import oosd.views.components.*;
+import oosd.views.factories.ViewComponentFactory;
+
+import java.util.HashMap;
 
 /**
  * SOLID:  Single Responsibility Principle
  * The view should only be responsible for managing the user interface (e.g. interacting with the JavaFX library)
  */
-public class BoardView extends View {
+public class BoardView implements View {
     private final Board board;
+    private final HashMap<Piece, SelectionPiecePolygon> selectionPieces;
+    private final HashMap<Piece, UnitPiecePolygon> unitPieces;
+    private final HashMap<Piece, DefendPieceImage> defendPieces;
+    private final HashMap<Piece, BackgroundPiecePolygon> backgroundPieces;
     private Pane windowGridPane;
-    private final StackPane boardPane;
+    private final BoardPane boardPane;
     private final GameController controller;
-    private final PieceViewComponent<Polygon> selectionPieces;
-    private final PieceViewComponent<Polygon> backgroundPieces;
-    private final PieceViewComponent<Polygon> unitPieces;
-    private final PieceViewComponent<ImageView> defendPieceImages;
     private final ViewComponentFactory boardFactory;
     private GameEngine gameEngine;
-    private Pane sidebar;
-    private HBox toolbar;
+    private SidebarPane sidebar;
+    private ToolbarPane toolbar;
     private Text playerTurn;
 
-    public BoardView(GameController controller, GameEngine gameEngine, Pane windowGridPane, StackPane boardPane, Pane sidebar, HBox toolbar) {
+    public BoardView(GameController controller, GameEngine gameEngine, WindowGridPane windowGridPane, BoardPane boardPane, SidebarPane sidebar, ToolbarPane toolbar) {
         this.controller = controller;
         this.gameEngine = gameEngine;
         this.windowGridPane = windowGridPane;
         this.boardPane = boardPane;
-        this.board = gameEngine.getBoard();
         this.sidebar = sidebar;
         this.toolbar = toolbar;
-        this.boardFactory = new ViewComponentFactory(board.getColumns(), board.getRows());
-        this.backgroundPieces = boardFactory.createPieces();
-        this.unitPieces = boardFactory.createPieces();
-        this.selectionPieces = boardFactory.createPieces();
-        this.defendPieceImages = boardFactory.createPieceImages("shield");
-        this.playerTurn = (Text) sidebar.lookup("#playerTurn");
+        this.board = gameEngine.getBoard();
+        this.boardFactory = new ViewComponentFactory(board);
+        this.unitPieces = boardFactory.createPiecePolygons(new UnitPiecePolygon());
+        this.selectionPieces = boardFactory.createPiecePolygons(new SelectionPiecePolygon());
+        this.defendPieces = boardFactory.createPiecePolygons(new DefendPieceImage());
+        this.backgroundPieces = boardFactory.createPiecePolygons(new BackgroundPiecePolygon());
+        this.playerTurn = sidebar.getPlayerTurnText();
     }
 
     public void moveUnit(Piece selectedPiece, Piece clickedPiece) {
         for (int yIndex = 0; yIndex < board.getRows(); yIndex++) {
             for (int xIndex = 0; xIndex < board.getColumns(); xIndex++) {
-                selectionPieces.getPiece(xIndex, yIndex).setVisible(false);
+                selectionPieces.get(board.getPiece(xIndex, yIndex)).setVisible(false);
             }
         }
 
-        selectionPieces.getPiece(selectedPiece).setOpacity(1.0);
-        selectionPieces.getPiece(selectedPiece).setVisible(false);
-        unitPieces.getPiece(selectedPiece).setVisible(false);
-        unitPieces.getPiece(clickedPiece).setVisible(true);
-        unitPieces.getPiece(clickedPiece).setFill(boardFactory.createImage(clickedPiece.getUnit().getImage()));
+        selectionPieces.get(selectedPiece).setOpacity(1.0);
+        selectionPieces.get(selectedPiece).setVisible(false);
+        unitPieces.get(selectedPiece).setVisible(false);
+        unitPieces.get(clickedPiece).setVisible(true);
+        unitPieces.get(clickedPiece).setFill(boardFactory.createImage(clickedPiece.getUnit().getImage()));
         playerTurn.setText("Player turn: " + gameEngine.getTurn().getPlayerName());
     }
 
     public void selectUnit(Piece selectedPiece, Piece clickedPiece) {
         if (selectedPiece != null) {
-            selectionPieces.getPiece(selectedPiece).setVisible(false);
+            selectionPieces.get(selectedPiece).setVisible(false);
 
             for (Piece piece : selectedPiece.getUnit().getUnitBehaviour().getValidMoves(gameEngine, selectedPiece)) {
-                selectionPieces.getPiece(piece).setVisible(false);
+                selectionPieces.get(piece).setVisible(false);
             }
         }
 
         for (Piece piece : clickedPiece.getUnit().getUnitBehaviour().getValidMoves(gameEngine, clickedPiece)) {
-            selectionPieces.getPiece(piece).setVisible(true);
-            selectionPieces.getPiece(piece).setFill(Paint.valueOf("#00C400"));
+            selectionPieces.get(piece).setVisible(true);
+            selectionPieces.get(piece).setFill(Paint.valueOf("#00C400"));
         }
 
-        selectionPieces.getPiece(clickedPiece).setFill(Paint.valueOf("#dadada"));
-        selectionPieces.getPiece(clickedPiece).setOpacity(0.5);
+        selectionPieces.get(clickedPiece).setFill(Paint.valueOf("#dadada"));
+        selectionPieces.get(clickedPiece).setOpacity(0.5);
     }
 
-    public void initialize() {
-        double x = 0;
-        double y = 0;
-        int pieceCount = 0;
-        Group group = new Group();
-
-        BackgroundImage backgroundImage = new BackgroundImage(new Image(View.class.getResource("menu.png").toString()), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(windowGridPane.getWidth(), windowGridPane.getHeight(), true, true, true, false));
-        Background background = new Background(backgroundImage);
-        windowGridPane.setBackground(background);
-
+    public void render() {
         playerTurn.setText("Player turn: " + gameEngine.getTurn().getPlayerName());
-
-        for (int yIndex = 0; yIndex < board.getRows(); yIndex++) {
-            for (int xIndex = 0; xIndex < board.getColumns(); xIndex++) {
-                Piece piece = board.getPiece(xIndex, yIndex);
-
-                if (piece.getUnit() != null) {
-                    unitPieces.getPiece(xIndex, yIndex).setFill(boardFactory.createImage(piece.getUnit().getImage()));
-                } else {
-                    unitPieces.getPiece(xIndex, yIndex).setVisible(false);
-                }
-
-                backgroundPieces.getPiece(xIndex, yIndex).setFill(boardFactory.createImage("grass"));
-                backgroundPieces.getPiece(xIndex, yIndex).setStrokeWidth(2);
-                backgroundPieces.getPiece(xIndex, yIndex).setStroke(Paint.valueOf("#706c1c"));
-
-                selectionPieces.getPiece(xIndex, yIndex).setVisible(false);
-
-                final StackPane stack = new StackPane();
-                stack.setOnMouseClicked(event -> handlePieceClick(event, piece));
-                stack.getChildren().addAll(backgroundPieces.getPiece(xIndex, yIndex), unitPieces.getPiece(xIndex, yIndex), selectionPieces.getPiece(xIndex, yIndex), defendPieceImages.getPiece(xIndex, yIndex));
-                stack.setLayoutX(x);
-
-                stack.setLayoutY(y);
-
-                group.getChildren().add(stack);
-
-                if (pieceCount % 2 == 0) {
-                    y = y + boardFactory.getHalfIncrement();
-                } else {
-                    y = y - boardFactory.getHalfIncrement();
-                }
-
-                pieceCount++;
-
-                x = xIndex == board.getColumns() - 1 ? 0 : x + boardFactory.getGap();
-            }
-
-            y = yIndex == board.getRows() - 1 ? 0 : y + boardFactory.getFullIncrement();
-        }
-
-        boardPane.getChildren().add(group);
+        boardPane.createBoard(gameEngine, controller, unitPieces, selectionPieces, defendPieces, backgroundPieces);
     }
 
     public void defendUnit(Piece piece) {
-        defendPieceImages.getPiece(piece).setVisible(true);
-    }
-
-    private void handlePieceClick(MouseEvent event, Piece piece) {
-        Piece selectedPiece = gameEngine.getSelectedPiece();
-
-        if (piece.getUnit() != null) {
-            if (!piece.getUnit().getPlayer().equals(gameEngine.getTurn())) {
-                return;
+        for (int yIndex = 0; yIndex < board.getRows(); yIndex++) {
+            for (int xIndex = 0; xIndex < board.getColumns(); xIndex++) {
+                selectionPieces.get(board.getPiece(xIndex, yIndex)).setVisible(false);
             }
-
-            controller.selectUnit(event, selectedPiece, piece);
-
-            return;
         }
 
-        if (selectedPiece != null) {
-            if (piece.equals(selectedPiece)) {
-                controller.defendUnit(event, piece);
-            }
-
-            if (!selectedPiece.getUnit().getUnitBehaviour().isValidMove(gameEngine, piece)) {
-                return;
-            }
-
-            controller.moveUnit(event, selectedPiece, piece);
-        }
+        defendPieces.get(piece).setVisible(true);
     }
 }
---
+++ src/main/java/oosd/views/factories/ViewComponentFactory.java
@@ -0,0 +1,44 @@
+package oosd.views.factories;
+
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
+import oosd.models.board.Board;
+import oosd.models.board.Piece;
+import oosd.views.View;
+
+import java.util.HashMap;
+
+/**
+ * GRASP: The creator
+ * This class is responsible for creating UI components in a board layout.
+ *
+ * GRASP: Information expert
+ * This factory should know the dimensions of the pieces on the board.
+ * No one else should alter or make different sized polygons other than this class.
+ *
+ * Design pattern: factory pattern used to produce custom view components for the UI.
+ */
+public class ViewComponentFactory {
+    private Board board;
+
+    public ViewComponentFactory(Board board) {
+        this.board = board;
+    }
+
+    public ImagePattern createImage(String imageName) {
+        Image image = new Image(View.class.getResource(imageName + ".png").toString());
+        return new ImagePattern(image);
+    }
+
+    public <T> HashMap<Piece, T> createPiecePolygons(T polygon) {
+        HashMap<Piece, T> pieces = new HashMap<>();
+
+        for (int yIndex = 0; yIndex < board.getRows(); yIndex++) {
+            for (int xIndex = 0; xIndex < board.getColumns(); xIndex++) {
+                pieces.put(board.getPiece(xIndex, yIndex), polygon);
+            }
+        }
+
+        return pieces;
+    }
+}
