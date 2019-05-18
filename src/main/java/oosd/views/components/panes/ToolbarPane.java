d7dd73bcdc85767da001694f4291e176ea848529
Johnny Huynh <thejohnnyhuy@gmail.com>
2019-05-18 23:00:56 +1000

    Updated undo and defend buttons and other images.

--- src/main/java/oosd/views/components/panes/ToolbarPane.java
+++ src/main/java/oosd/views/components/panes/ToolbarPane.java
@@ -4,11 +4,17 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import oosd.controllers.GameController;
 import oosd.models.GameEngine;
+import oosd.views.components.images.DefendButtonImage;
+import oosd.views.components.images.UndoButtonImage;
 import oosd.views.handlers.UndoClickHandler;
 
 public class ToolbarPane extends HBox {
     public void initialise(GameEngine gameEngine, GameController gameController) {
         Button undoButton = (Button) this.lookup("#undoButton");
         undoButton.setOnMouseClicked(new UndoClickHandler(gameEngine, gameController));
+        undoButton.setGraphic(new UndoButtonImage());
+        Button defendButton = (Button) this.lookup("#defendButton");
+        defendButton.setOnMouseClicked(new UndoClickHandler(gameEngine, gameController));
+        defendButton.setGraphic(new DefendButtonImage());
     }
 }
