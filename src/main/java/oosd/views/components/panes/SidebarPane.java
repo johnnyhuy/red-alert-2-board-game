cdf7ab5aff376508b7a21a2eec05891eea354389
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-05-24 11:34:56 +1000

    Added base sidebar layout.

--- src/main/java/oosd/views/components/panes/SidebarPane.java
+++ src/main/java/oosd/views/components/panes/SidebarPane.java
@@ -1,15 +1,19 @@
 package oosd.views.components.panes;
 
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import oosd.models.game.Engine;
 import oosd.models.player.Player;
 
 import static oosd.helpers.ObjectHelper.exists;
 
-public class SidebarPane extends Pane {
+public class SidebarPane extends AnchorPane {
     public Text getPlayerTurnText() {
         return (Text) this.lookup("#playerTurn");
+    }
+
+    public Text getPlayerTurnCountText() {
+        return (Text) this.lookup("#playerTurnCount");
     }
 
     public void initialise(Engine engine) {
