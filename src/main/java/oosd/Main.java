4a9cd3bafff9050c87148ffef0e6f4a1e750c1e6
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2019-05-26 14:16:57 +1000

    Moved loader.

--- src/main/java/oosd/Main.java
+++ src/main/java/oosd/Main.java
@@ -1,16 +1,11 @@
 package oosd;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import oosd.controllers.Controller;
 import oosd.views.View;
-import oosd.views.components.alerts.ErrorAlert;
 import oosd.views.components.panes.GameWindowPane;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Objects;
 
@@ -39,23 +34,11 @@ public class Main extends Application {
      */
     @Override
     public void start(Stage stage) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
-        GameWindowPane gameWindowPane = new GameWindowPane();
-
         final String windowTitle = "Red Alert 2 Board Game";
         final String windowIcon = "soviet.png";
         final String styles = "style/main.css";
 
-        FXMLLoader loader = new FXMLLoader(Controller.class.getResource("board.fxml"));
-        loader.setRoot(gameWindowPane);
-        loader.setControllerFactory(context::getBean);
-
-        try {
-            loader.load();
-        } catch (Exception exception) {
-            ErrorAlert errorAlert = new ErrorAlert();
-            errorAlert.exit();
-        }
+        GameWindowPane gameWindowPane = new GameWindowPane();
 
         Scene content = new Scene(gameWindowPane, 1200, 900);
         content.getStylesheets().add(Objects.requireNonNull(getClass().getClassLoader().getResource(styles)).toString());
--- src/main/java/oosd/views/components/panes/GameWindowPane.java
+++ src/main/java/oosd/views/components/panes/GameWindowPane.java
@@ -1,8 +1,14 @@
 package oosd.views.components.panes;
 
+import javafx.fxml.FXMLLoader;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
+import oosd.Config;
+import oosd.controllers.Controller;
 import oosd.views.View;
+import oosd.views.components.alerts.ErrorAlert;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class GameWindowPane extends BorderPane {
     public GameWindowPane() {
@@ -12,5 +18,17 @@ public class GameWindowPane extends BorderPane {
         Background background = new Background(backgroundImage);
 
         this.setBackground(background);
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        FXMLLoader loader = new FXMLLoader(Controller.class.getResource("board.fxml"));
+        loader.setRoot(this);
+        loader.setControllerFactory(context::getBean);
+
+        try {
+            loader.load();
+        } catch (Exception exception) {
+            ErrorAlert errorAlert = new ErrorAlert();
+            errorAlert.exit();
+        }
     }
 }
